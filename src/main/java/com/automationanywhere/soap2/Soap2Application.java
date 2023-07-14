3cdb4f3233bca531b98337234a995471e86afce0
Utkarsh Singh <utkarsh.s@automationanywhere.com>
2023-07-14 12:36:05 +0530

    first commit

---
+++ src/main/java/com/automationanywhere/soap2/Soap2Application.java
@@ -0,0 +1,13 @@
+package com.automationanywhere.soap2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soap2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soap2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/automationanywhere/soap2/WsdlController.java
@@ -0,0 +1,33 @@
+package com.automationanywhere.soap2;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+
+@RestController
+public class WsdlController {
+
+    @GetMapping("/wsdl")
+    public void getWsdl(HttpServletResponse response) throws IOException {
+        // Load the merged WSDL file from the classpath
+        ClassPathResource wsdlResource = new ClassPathResource("merged_wsdl.xml");
+        InputStream inputStream = wsdlResource.getInputStream();
+
+        // Set the response headers
+        response.setContentType("text/xml");
+        response.setHeader("Content-Disposition", "attachment; filename=\"merged_wsdl.xml\"");
+
+        // Write the WSDL file to the response
+        int bytesRead;
+        byte[] buffer = new byte[4096];
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            response.getOutputStream().write(buffer, 0, bytesRead);
+        }
+        response.flushBuffer();
+    }
+}
+
